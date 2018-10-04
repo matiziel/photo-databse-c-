247f2bec379069f432b6b3999af7a9ccb09702e3
Mateusz <matiziel10@gmail.com>
2018-10-04 11:47:57 +0200

    Dodanie klasy Photo

--- Photo.cs
+++ Photo.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace photodatabase
 {
-    class Program
+    class Photo
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
     }
 }
